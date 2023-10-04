e2d6b5bcc966ebc2d98506bc024ae4838a691716
Кирилл Евстигнеев <creepyawooga.ev@gmail.com>
2023-10-04 13:12:38 +0500

    ошибки

--- js/functions.js
+++ js/functions.js
@@ -2,7 +2,7 @@ function checkLengthStr(str, maxlength) {
   return str.length <= maxlength;
 }
 
-console.log(checkLengthStr("Привет", 6));
+console.log(checkLengthStr('Привет', 6));
 console.log(checkLengthStr('проверяемая строка', 18));
 console.log(checkLengthStr('проверяемая строка', 10));
 function palindrom(str) {
@@ -10,5 +10,8 @@ function palindrom(str) {
   const reversedStr = str.split('').reverse().join('');
   return(str === reversedStr);
 }
-console.log(palindrom('Лёша на полке клопа нашёл '));
 
+console.log(checkLengthStr("Привет", 6));
+console.log(checkLengthStr('проверяемая строка', 18));
+console.log(checkLengthStr('проверяемая строка', 10));
+console.log(palindrom('Лёша на полке клопа нашёл '));
